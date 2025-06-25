433c2acd54a936c94dfe9bc0c266627bfe26589a
CristianV22 <cristian.v.s.oliveira@gmail.com>
2025-06-25 13:29:59 -0300

    Separation BE

--- frontend/src/App.jsx
+++
@@ -1,8 +0,0 @@
-import './App.css'
-import { TodoListView } from './views/TodoListView';
-
-function App() {
-  return <TodoListView />;
-}
-
-export default App
--- frontend/src/models/todoModel.js
+++
@@ -1,7 +0,0 @@
-import { api } from '../services/api';
-
-export const todoModel = {
-  getAll: () => api.get('/todos'),
-  create: (title) => api.post('/todos', { title }),
-  remove: (id) => api.delete(`/todos/${id}`)
-};
