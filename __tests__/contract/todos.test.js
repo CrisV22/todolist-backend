45627ef2a35f61b057d140c07fce082fc6ee0f89
CristianV22 <cristian.v.s.oliveira@gmail.com>
2025-07-07 21:30:07 -0300

    Testing smoke api testing

--- __tests__/contract/todos.test.js
+++ __tests__/contract/todos.test.js
@@ -7,22 +7,28 @@ describe('Todo CRUD contract test', () => {
     let res;
     const path = '/todos';
 
-    it('POST response shall be according to the schema', async () => {
-        let data = todoData();
-        res = await postRequest(path, data);
-        validateSchema(res, postTodoSchema);
-    });
+    it('Deve retornar "listening"', async () => {
+    const res = await request(app).get('/')
+    expect(res.statusCode).toEqual(200)
+    expect(res.body).toEqual('listening')
+  })
 
-    it('GET response shall be according to the schema', async () => {
-        res = await getRequest(path);
-        validateSchema(res, getTodosSchema);
-    });
+    // it('POST response shall be according to the schema', async () => {
+    //     let data = todoData();
+    //     res = await postRequest(path, data);
+    //     validateSchema(res, postTodoSchema);
+    // });
 
-    it('DELETE response shall be according to the schema', async () => {        
-        if (!res || res.length === 0) throw new Error("No todos found to delete");
+    // it('GET response shall be according to the schema', async () => {
+    //     res = await getRequest(path);
+    //     validateSchema(res, getTodosSchema);
+    // });
+
+    // it('DELETE response shall be according to the schema', async () => {        
+    //     if (!res || res.length === 0) throw new Error("No todos found to delete");
         
-        const todoId = res[0].id;
-        res = await deleteRequest(`${path}/${todoId}`);
-        validateSchema(res, deleteTodoSchema);
-    });
+    //     const todoId = res[0].id;
+    //     res = await deleteRequest(`${path}/${todoId}`);
+    //     validateSchema(res, deleteTodoSchema);
+    // });
 })
