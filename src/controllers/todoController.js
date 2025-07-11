8aa9bc5a34c101cc941e894c40c924841bfd7d46
CristianV22 <cristian.v.s.oliveira@gmail.com>
2025-07-11 14:46:31 -0300

    Presentation

--- src/controllers/todoController.js
+++ src/controllers/todoController.js
@@ -1,7 +1,7 @@
 const todoModel = require('../models/todoModel');
 
 const getHealthCheck = async (req, res) => {
-  res.json("listening");
+  res.json("listening updated!");
 };
 
 const getTodos = async (req, res) => {
